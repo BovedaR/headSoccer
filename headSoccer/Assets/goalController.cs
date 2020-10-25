6038e095338dd0f67e70c614013bd70a219d139f
tomas <tbvescio@gmail.com>
2020-10-25 17:02:23 -0300

    mejoras

---
+++ headSoccer/Assets/goalController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goalController : MonoBehaviour
+{
+    public static int player1Goals = 0;
+    public static int player2Goals = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.name == "Ball")
+        {
+            if (this.name == "goal_1")
+            {
+                player1Goals++;
+                Debug.Log(player1Goals);
+            }
+            else
+            {
+                player2Goals++;
+                Debug.Log(player2Goals);
+            }
+        
+        }
+    } 
+}
---
+++ headSoccer/Assets/textController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class textController : MonoBehaviour
+{
+    Text instruction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instruction = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.name == "player1Text")
+        {
+            instruction.text = goalController.player1Goals.ToString();
+        }
+        else
+        {
+            instruction.text  = goalController.player2Goals.ToString();
+        }
+    }
+}
