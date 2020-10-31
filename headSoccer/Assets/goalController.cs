40a174dafd703065f7fff03e8b272f6547c0abc3
tomas <tbvescio@gmail.com>
2020-10-31 15:48:59 -0300

    reset after goal

--- headSoccer/Assets/goalController.cs
+++ headSoccer/Assets/goalController.cs
@@ -7,20 +7,25 @@ public class goalController : MonoBehaviour
     public static int player1Goals = 0;
     public static int player2Goals = 0;
 
+    GameObject Ball;
+    GameObject Player1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Ball = GameObject.Find("Ball");
+        Player1 = GameObject.Find("Player1");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
 
-    void OnTriggerEnter2D(Collider2D other) {
+    void OnTriggerEnter2D(Collider2D other)
+    {
         if (other.gameObject.name == "Ball")
         {
             if (this.name == "goal_1")
@@ -33,7 +38,14 @@ public class goalController : MonoBehaviour
                 player2Goals++;
                 Debug.Log(player2Goals);
             }
-        
+
+            //TODO: Player2
+            Player1.transform.position = new Vector3(-6, -4, 0);
+
+            //TODO: Congelar pelota X tiempo
+            Ball.transform.position = new Vector3(0, 3, 0);
+
         }
-    } 
+    }
+
 }
